bd4969f445182a1113cf403a91e444fc3b48a4e7
chovin <152864419+Ch0vin@users.noreply.github.com>
2025-06-28 23:48:16 +0300

    building JwtService

--- demo/src/main/java/com/example/demo/repository/UserRepository.java
+++ demo/src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,7 +1,13 @@
 package com.example.demo.repository;
 
+import com.example.demo.model.User;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository {
+public interface UserRepository extends CrudRepository<User , Long> {
+    Optional<User> findByEamil(String email);
+    Optional<User> findByVerificatiobCode(String verficationcode);
 }
